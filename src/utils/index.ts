f5ec75edbf94f7e738728f60fd00479be2bb2816
studio-firefly-work <account@studio-firefly.com>
2024-05-08 18:09:37 +0900

    ページごとのSEO関連情報をconstantsに移動

---
+++ src/utils/index.ts
@@ -0,0 +1,16 @@
+/**
+ * 電話番号から記号を取り除く
+ * @param telNumber 電話番号
+ * @returns 記号を取り除いた電話番号
+ */
+export function normalizeTelNumber(telNumber: string): string {
+  // 数字と記号以外の文字が含まれているかチェック
+  if (!/^[\d()-]+$/.test(telNumber)) {
+    throw new Error('Invalid tel number format. Only digits, hyphens, and parentheses are allowed.');
+  }
+
+  // 電話番号から数字以外の文字を取り除く
+  const normalizedTelNumber = telNumber.replace(/\D/g, '');
+
+  return normalizedTelNumber;
+}
