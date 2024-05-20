e2fc741830d8de27e4a6438b90c66c7067942065
ikuji_canvas <k.ikuji@canvas-works.jp>
2024-05-20 14:54:11 +0900

    tailwindに置換

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,33 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#e0ca72',
+        'secondary': '#d6caa5',
+        'tertiary': '#add4b7',
+        'error': '#ffbab1',
+      },
+    },
+    fontFamily: {
+      body: [
+        '"Noto Sans"',
+        '"Noto Sans JP"',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Hiragino Sans"',
+        '"Noto Sans CJK JP"',
+        '"Original Yu Gothic"',
+        '"Yu Gothic"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Sans Emoji"'
+      ]
+    }
   },
   plugins: [],
 }
