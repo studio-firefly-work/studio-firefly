0aa25683d301732d813c727093db3b2e32d52bd5
ikuji_canvas <k.ikuji@canvas-works.jp>
2024-06-06 09:38:42 +0900

    astro-icon del

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
-import icon from "astro-icon";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
