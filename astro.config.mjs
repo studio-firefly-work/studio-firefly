397aedf61a08b2b482b7aceeb509a79eafdc3176
studio-firefly-work <account@studio-firefly.com>
2024-06-07 09:16:18 +0900

    prettierで整形

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import node from '@astrojs/node'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
