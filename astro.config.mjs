14227abed50c5341609e61faddccf76491e53a3e
Your Name <you@example.com>
2024-07-25 21:49:08 +0900

    add view transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config'
-import daisyui from "daisyui"
+import daisyui from 'daisyui'
 import node from '@astrojs/node'
 import tailwind from '@astrojs/tailwind'
 
@@ -19,7 +19,5 @@ export default defineConfig({
     },
   },
   integrations: [tailwind()],
-  plugins: [
-    daisyui,
-  ],
+  plugins: [daisyui],
 })
