743288dda0065167fdea77b08faf3616932ab33e
ikuji_canvas <k.ikuji@canvas-works.jp>
2024-07-29 16:05:34 +0900

    update seo

--- src/constants/pageDatas.ts
+++ src/constants/pageDatas.ts
@@ -2,58 +2,58 @@ export type PageData = {
   permalink: string
   title: Title
   description: string
-  keyword: string
+  type: string
   noindex?: boolean
 }
 
 export type Title = {
-  jp: string
-  en: string
+  jp: string | null
+  en: string | null
 }
 
 export const pageDatas: PageData[] = [
   {
     permalink: '/',
-    title: { jp: 'ホーム', en: 'Home' },
+    title: { jp: '', en: '' },
+    type: "website",
     description: 'description test',
-    keyword: 'keyword test',
   },
   {
     permalink: '/profile',
     title: { jp: '自己紹介', en: 'Profile' },
+    type: "article",
     description: 'description test',
-    keyword: 'keyword test',
   },
   {
     permalink: '/skill',
     title: { jp: '技術', en: 'Skill' },
+    type: "article",
     description: 'description test',
-    keyword: 'keyword test',
   },
   {
     permalink: '/works',
     title: { jp: '制作実績', en: 'Works' },
+    type: "article",
     description: 'description test',
-    keyword: 'keyword test',
   },
   {
     permalink: '/contact',
     title: { jp: 'お問い合わせ', en: 'Contact' },
+    type: "article",
     description: 'description test',
-    keyword: 'keyword test',
   },
   {
     permalink: '/thanks',
     title: { jp: '', en: 'Thanks' },
+    type: "article",
     description: 'description test',
-    keyword: 'keyword test',
     noindex: true,
   },
   {
     permalink: '/test',
     title: { jp: 'テスト', en: 'Test' },
+    type: "article",
     description: 'description test',
-    keyword: 'keyword test'
   },
 ]
 
