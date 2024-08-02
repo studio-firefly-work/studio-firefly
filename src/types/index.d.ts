5ddba1c8271c90f0c6bfc3cc8ac2a6212fb2d9b2
ikuji_canvas <k.ikuji@canvas-works.jp>
2024-08-02 10:47:23 +0900

    たどり着いたSEO

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,42 +1,40 @@
 declare namespace Theme {
-  namespace Page {
-    interface Config {
-      title: string
-      description: string
-      permalink: string
-      noindex?: boolean
-      nofollow?: boolean
-      type?: string
-      image?: string
-      alt?: string
-      article?: {
-        publishedTime?: string
-        modifiedTime?: string
-        expirationTime?: string
-        authors?: string[]
-        section?: string
-        tags?: string[]
-      }
-      twitter?: {
-        card?: TwitterCardType
-        site?: string
-        creator?: string
-      }
-      extend?: {
-        link?: Partial<Link>[]
-        meta?: Partial<Meta>[]
-      }
-      schemaOrg?: Array[]
+  type SEO = {
+    title: string
+    description: string
+    permalink: string
+    noindex?: boolean
+    nofollow?: boolean
+    type?: string
+    image?: string
+    alt?: string
+    article?: {
+      publishedTime?: string
+      modifiedTime?: string
+      expirationTime?: string
+      authors?: string[]
+      section?: string
+      tags?: string[]
     }
+    twitter?: {
+      card?: TwitterCardType
+      site?: string
+      creator?: string
+    }
+    extend?: {
+      link?: Partial<Link>[]
+      meta?: Partial<Meta>[]
+    }
+    schemaOrg?: Array[]
+  }
 
-    interface Link extends Omit<HTMLLinkElement, 'sizes'> {
-      prefetch: boolean;
-      crossorigin: string;
-      sizes: string;
-    }
+  interface Link extends Omit<HTMLLinkElement, 'sizes'> {
+    prefetch: boolean;
+    crossorigin: string;
+    sizes: string;
+  }
 
-    interface Meta extends HTMLMetaElement {
-      property: string;
-    }
+  interface Meta extends HTMLMetaElement {
+    property: string;
   }
 }
