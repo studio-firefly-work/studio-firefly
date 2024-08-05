58c2c7a6ee4603f306e4b132dbbf84ebdec92d1d
Your Name <you@example.com>
2024-08-05 23:00:07 +0900

    add react-hook-form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
-import { defineConfig } from 'astro/config'
-import { loadEnv } from "vite"
-import daisyui from 'daisyui'
-import node from '@astrojs/node'
-import partytown from "@astrojs/partytown"
-import sitemap from "@astrojs/sitemap"
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import { loadEnv } from "vite";
+import daisyui from 'daisyui';
+import node from '@astrojs/node';
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import tailwind from '@astrojs/tailwind';
+import react from "@astrojs/react";
+const {
+  SITE,
+  BASE
+} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
-const { SITE, BASE } = loadEnv(process.env.NODE_ENV, process.cwd(), "")
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,9 +20,9 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone'
   }),
-  integrations: [partytown(), sitemap(), tailwind()],
+  integrations: [partytown(), sitemap(), tailwind(), react()],
   plugins: [daisyui],
   server: {
     host: true
   }
-})
+});
