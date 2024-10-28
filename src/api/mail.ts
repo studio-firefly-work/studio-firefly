9ad071692da44480c158ab2b75a716f6636a3261
ikuji_canvas <k.ikuji@canvas-works.jp>
2024-10-28 17:05:48 +0900

    add recaptcha

--- src/api/mail.ts
+++ src/api/mail.ts
@@ -3,21 +3,29 @@
  */
 export const sendMail = async (data: any) => {
   try {
+    const recaptchaToken = await new Promise((resolve) => {
+      grecaptcha.ready(() => {
+        grecaptcha.execute(import.meta.env.PUBLIC_RECAPTCHA_SITE_KEY, { action: "contact" })
+          .then(resolve)
+      });
+    });
+
     const res = await fetch(`${import.meta.env.PUBLIC_API_BASE_URL}/mail/send/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data),
-    })
+      body: JSON.stringify({ ...data, recaptchaToken }),
+    });
 
     if (!res.ok) {
-      console.error('サーバーエラー')
+      console.error('サーバーエラー');
     } else {
-      console.log('送信が正常に完了しました')
+      console.log('送信が正常に完了しました');
     }
-    return res
+
+    return res;
   } catch (error) {
-    console.error('通信に失敗しました', error)
+    console.error('通信に失敗しました', error);
   }
 }
