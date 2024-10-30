e5f4e11209ab8b664346becaabb52c74c7f0b5f8
ikuji_canvas <k.ikuji@canvas-works.jp>
2024-10-30 11:59:37 +0900

    Supports SSL certificates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config'
 import { loadEnv } from 'vite'
+import fs from 'fs';
 import daisyui from 'daisyui'
 import node from '@astrojs/node'
 import partytown from '@astrojs/partytown'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-const { SITE, BASE } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
+const { SITE, BASE, SSL_KEY_PATH, SSL_CERT_PATH } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
+
+const httpsOptions = {
+  key: fs.readFileSync(SSL_KEY_PATH), // プライベートキーの読み込み
+  cert: fs.readFileSync(SSL_CERT_PATH), // 証明書の読み込み
+}
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,5 +26,10 @@ export default defineConfig({
   plugins: [daisyui],
   server: {
     host: true,
+  },
+  vite: {
+    server: {
+      https: httpsOptions
+    }
   },
 })
