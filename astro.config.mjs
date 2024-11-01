aac11ac9844ffcd51588b6c5f9118cf173d92b7f
ikuji_canvas <k.ikuji@canvas-works.jp>
2024-11-01 18:12:56 +0900

    add vitest test code

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,7 @@ import partytown from '@astrojs/partytown'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-const { SITE, BASE, SSL_KEY_PATH, SSL_CERT_PATH } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
-
-const httpsOptions = {
-  key: fs.readFileSync(SSL_KEY_PATH), // プライベートキーの読み込み
-  cert: fs.readFileSync(SSL_CERT_PATH), // 証明書の読み込み
-}
+const { SITE, BASE } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
 
 // https://astro.build/config
 export default defineConfig({
@@ -29,7 +24,10 @@ export default defineConfig({
   },
   vite: {
     server: {
-      https: httpsOptions
+      https: {
+        key: fs.readFileSync("./localhost-key.pem"),
+        cert: fs.readFileSync("./localhost.pem")
+      }
     }
   },
 })
